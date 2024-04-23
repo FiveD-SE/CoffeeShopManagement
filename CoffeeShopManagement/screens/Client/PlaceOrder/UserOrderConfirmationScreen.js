58d35402cb64dde4ff733f4af7f8a85fdf60771f
not-thanhtai <tai606760@gmail.com>
2024-04-24 03:26:20 +0700

    feat: Update ApplyVoucherButton, ChooseDelivery

--- CoffeeShopManagement/screens/Client/PlaceOrder/UserOrderConfirmationScreen.js
+++ CoffeeShopManagement/screens/Client/PlaceOrder/UserOrderConfirmationScreen.js
@@ -9,6 +9,9 @@ import TotalOrderList from "../../../components/Client/List/TotalOrderList";
 import PaymentMethodButton from "../../../components/Client/Button/PaymentMethodButton";
 import ChooseDeliveryButton from "../../../components/Client/Button/ChooseDeliveryButton";
 import ChooseDeliveryBottomSheet from "../../../components/Client/BottomSheet/ChooseDeliveryBottomSheet";
+import DeliveryInformationButton from "../../../components/Client/Button/DeliveryInformationButton";
+import ChooseCouponBottomSheet from "../../../components/Client/BottomSheet/ChooseCouponBottomSheet";
+import OrderConfirmationFooter from "../../../components/Client/OrderConfirmationFooter";
 
 const CASH_ICON = require("../../../assets/cash.png");
 const CREDIT_CARD_ICON = require("../../../assets/credit-card.png");
@@ -16,13 +19,50 @@ const MOMO_ICON = require("../../../assets/momo.png");
 
 const UserOrderConfirmationScreen = () => {
 	const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+	const [deliveryOption, setDeliveryOption] = useState("takeaway");
+
 	const [isOpen, setIsOpen] = useState(false);
 	const chooseDeliverySnapPoints = useMemo(() => ["30%"], []);
 	const chooseDeliveryBottomSheetRef = useRef(null);
+
+	const chooseCouponSnapPoints = useMemo(() => ["85%"], []);
+	const chooseCouponBottomSheetRef = useRef(null);
+
 	const paymentMethods = {
 		cash: { title: "Tiền mặt", imageSource: CASH_ICON },
 		creditCard: { title: "Thẻ ngân hàng", imageSource: CREDIT_CARD_ICON },
 		momo: { title: "Momo", imageSource: MOMO_ICON },
+	};
+
+	const deliveryMethod = {
+		takeaway: {
+			branch: {
+				title: "Chi nhánh",
+				details: "66E Hoàng Diệu 2, Quận Thủ Đức, Hồ Chí Minh, Việt Nam",
+			},
+			receiver: {
+				title: "Thông tin người nhận",
+				details: "Tánh Chần (0352085655)",
+			},
+			deliveryTime: {
+				title: "Thời gian nhận hàng",
+				details: "15 - 30 phút (Càng sớm càng tốt)",
+			},
+		},
+		delivery: {
+			address: {
+				title: "Địa chỉ giao hàng",
+				details: "KTX khu B, Đông Hoà, Dĩ An, Bình Dương",
+			},
+			receiver: {
+				title: "Thông tin người nhận",
+				details: "Tánh Chần (0352085655)",
+			},
+			deliveryTime: {
+				title: "Thời gian nhận hàng",
+				details: "15 - 30 phút (Càng sớm càng tốt)",
+			},
+		},
 	};
 
 	const handlePaymentMethodChange = (method) => {
@@ -31,6 +71,11 @@ const UserOrderConfirmationScreen = () => {
 
 	const handleChooseDeliveryMethod = () => {
 		chooseDeliveryBottomSheetRef.current?.present();
+		setIsOpen(true);
+	};
+
+	const handleChooseCoupon = () => {
+		chooseCouponBottomSheetRef.current?.present();
 		setIsOpen(true);
 	};
 
@@ -51,14 +96,59 @@ const UserOrderConfirmationScreen = () => {
 		});
 	};
 
+	const renderTakeawayList = () => {
+		if (deliveryOption === "takeaway") {
+			const takeaway = deliveryMethod.takeaway;
+
+			return Object.keys(takeaway).map((index) => {
+				const { title, details } = takeaway[index];
+				return (
+					<View key={index} style={{ marginTop: "5%" }}>
+						<DeliveryInformationButton title={title} details={details} />
+					</View>
+				);
+			});
+		}
+		return null;
+	};
+
+	const renderDeliveryList = () => {
+		if (deliveryOption === "delivery") {
+			const delivery = deliveryMethod.delivery;
+
+			return Object.keys(delivery).map((index) => {
+				const { title, details } = delivery[index];
+				return (
+					<View key={index} style={{ marginTop: "5%" }}>
+						<DeliveryInformationButton title={title} details={details} />
+					</View>
+				);
+			});
+		}
+		return null;
+	};
+
+	const setChooseDeliveryButtonTitle = () => {
+		if (deliveryOption === "takeaway") {
+			return "Tự đến lấy hàng";
+		} else if (deliveryOption === "delivery") {
+			return "Giao hàng tận nơi";
+		}
+		return "Chọn phương thức đặt hàng";
+	};
+
 	return (
 		<BottomSheetModalProvider>
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<View style={styles.container} showsVerticalScrollIndicator={false}>
 					<ChooseDeliveryButton
-						title={"Chọn phương thức đặt hàng"}
+						title={setChooseDeliveryButtonTitle()}
 						onPress={handleChooseDeliveryMethod}
 					/>
+
+					{renderTakeawayList()}
+					{renderDeliveryList()}
+
 					<View style={{ marginTop: "5%" }}>
 						<Section title="Sản phẩm đã chọn">
 							<View style={{ marginTop: "2%" }}>
@@ -67,7 +157,7 @@ const UserOrderConfirmationScreen = () => {
 						</Section>
 					</View>
 					<View style={{ marginTop: "5%" }}>
-						<ApplyVoucherButton />
+						<ApplyVoucherButton onPress={handleChooseCoupon} />
 					</View>
 					<View style={{ marginTop: "5%" }}>
 						<Section title="Tổng cộng">
@@ -83,23 +173,15 @@ const UserOrderConfirmationScreen = () => {
 					</View>
 				</View>
 			</ScrollView>
-			<View style={styles.footer}>
-				<View style={styles.totalPriceContainer}>
-					<Text style={styles.label}>Tổng cộng:</Text>
-					<Text style={styles.price}>
-						{(10000).toLocaleString("vi-VN", {
-							style: "currency",
-							currency: "VND",
-						})}
-					</Text>
-				</View>
-				<Pressable style={styles.orderButton}>
-					<Text style={styles.orderButtonText}>Đặt hàng</Text>
-				</Pressable>
-			</View>
+			<OrderConfirmationFooter />
 			<ChooseDeliveryBottomSheet
 				bottomSheetRef={chooseDeliveryBottomSheetRef}
 				snapPoints={chooseDeliverySnapPoints}
+				setIsOpen={setIsOpen}
+			/>
+			<ChooseCouponBottomSheet
+				bottomSheetRef={chooseCouponBottomSheetRef}
+				snapPoints={chooseCouponSnapPoints}
 				setIsOpen={setIsOpen}
 			/>
 		</BottomSheetModalProvider>
@@ -139,39 +221,5 @@ const styles = StyleSheet.create({
 		color: "#00A188",
 		fontSize: 14,
 		fontWeight: "700",
-	},
-
-	footer: {
-		backgroundColor: "#FFFFFF",
-		paddingHorizontal: "5%",
-		paddingVertical: "2%",
-	},
-	totalPriceContainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	label: {
-		color: "#3a3a3a",
-		fontSize: 16,
-		fontWeight: "700",
-	},
-	price: {
-		color: "#3a3a3a",
-		fontSize: 20,
-		fontWeight: "700",
-	},
-	orderButton: {
-		backgroundColor: "#00A188",
-		justifyContent: "center",
-		alignItems: "center",
-		paddingVertical: "4%",
-		borderRadius: 20,
-		marginTop: "5%",
-	},
-	orderButtonText: {
-		color: "#FFFFFF",
-		fontSize: 16,
-		fontWeight: "600",
 	},
 });
