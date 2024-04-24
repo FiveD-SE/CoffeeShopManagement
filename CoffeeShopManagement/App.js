389c643a0863d0c34b1eccb16bec2f6898646eb4
sloweyinuit <vinhphucboyprogunner@gmail.com>
2024-04-24 19:20:00 +0700

    database setup

--- CoffeeShopManagement/App.js
+++ CoffeeShopManagement/App.js
@@ -1,25 +1,59 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import OnBoardingScreen from "./screens/Client/OnBoardingScreen";
 import AppNavigator from "./navigation/AppNavigator";
 import { useFonts } from "expo-font";
+import AuthNavigator from "./navigation/AuthNavigator";
+import connectDB from "./services/database";
 
 export default function App() {
+    const [role, setRole] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/connect-db")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.message);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error connecting to database:", error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/role")
+            .then((response) => response.json())
+            .then((data) => {
+                setRole(data.role);
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
+
     const [loaded] = useFonts({
         "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
         "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
         "Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
     });
-    const role = "cashier";
 
     if (!loaded) {
         return null;
     }
 
-    return <AppNavigator role={role} />;
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                {role ? <AppNavigator /> : <AuthNavigator />}
+            </NavigationContainer>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -28,11 +62,5 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
-    },
-
-    text: {
-        marginTop: 250,
-        fontSize: 20,
-        color: "black",
     },
 });
