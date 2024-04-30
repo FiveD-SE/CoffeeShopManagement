038ec5de5342616926512124f59f55a7136d7199
not-thanhtai <tai606760@gmail.com>
2024-05-01 00:03:53 +0700

    feat: Implement persistent storage for favorite

--- CoffeeShopManagement/App.js
+++ CoffeeShopManagement/App.js
@@ -10,33 +10,33 @@ import SignInScreen from "./screens/Client/SignInScreen";
 import { Provider, useSelector } from "react-redux";
 import store from "./redux/store/store";
 import { saveToken, getToken, removeToken } from "./services/authServices";
-
+import { initializeFavorites } from "./services/favoritesService";
 export default function App() {
-    const [role, setRole] = useState("");
+	const [role, setRole] = useState("");
 
-    const [loaded] = useFonts({
-        "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
-        "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
-        "Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
-    });
+	const [loaded] = useFonts({
+		"Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+		"Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+		"Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
+	});
 
-    useEffect(() => {
-        const getRole = async () => {
-            const token = await getToken();
-            if (token) {
-                setRole(token);
-            }
-        };
-        getRole();
-    }, []);
-    if (loaded) {
-        return (
-            <Provider store={store}>
-                <NavigationContainer>
-                    {role ? <AppNavigator role={role} /> : <AuthNavigator />}
-                </NavigationContainer>
-            </Provider>
-        );
-    }
-
+	useEffect(() => {
+		const getRole = async () => {
+			const token = await getToken();
+			if (token) {
+				setRole(token);
+			}
+		};
+		getRole();
+		initializeFavorites(store.dispatch);
+	}, []);
+	if (loaded) {
+		return (
+			<Provider store={store}>
+				<NavigationContainer>
+					{role ? <AppNavigator role={role} /> : <AuthNavigator />}
+				</NavigationContainer>
+			</Provider>
+		);
+	}
 }
