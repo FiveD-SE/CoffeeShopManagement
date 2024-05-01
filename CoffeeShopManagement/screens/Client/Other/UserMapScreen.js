287dad0b46e9d8b0a660af2874906e02718f4153
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2024-05-01 15:31:15 +0700

    Reapply "SelectPositionScreen Update"
    This reverts commit c583095e528453118c1c2009ceecab2d59d7f07a.

---
+++ CoffeeShopManagement/screens/Client/Other/UserMapScreen.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+export default function UserMapScreen() {
+  return (
+    <View style={styles.container}>
+        <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            region={{
+              region: {
+                latitude: 10.8231,
+                longitude: 106.6297,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+              }
+            }}
+        >
+        </MapView>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+    ...StyleSheet.absoluteFillObject,
+    },
+});
