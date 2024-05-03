27c13ddb0fc72107b39eece17d1e880cfe1fbef1
not-thanhtai <tai606760@gmail.com>
2024-05-04 00:52:40 +0700

    Update GET product list

--- CoffeeShopManagement/index.js
+++ CoffeeShopManagement/index.js
@@ -167,6 +167,16 @@ const startServer = () => {
 
 	const Product = mongoose.model("Product", productSchema);
 
+	app.get("/products", async (req, res) => {
+		try {
+			const products = await Product.find();
+			res.status(200).json(products);
+		} catch (error) {
+			console.error("Error fetching products", error);
+			res.status(500).json({ message: "Internal server error" });
+		}
+	});
+
 	app.get("/products/:productId", async (req, res) => {
 		try {
 			const productId = req.params.productId;
