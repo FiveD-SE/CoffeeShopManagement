7ad232b2d47d1d4a31c23b4b1a371e11dfbbd814
TrTueTah <henrytsuki@gmail.com>
2024-05-20 20:17:49 +0700

    Fix noti screen

---
+++ CoffeeShopManagement/assets/colors/index.js
@@ -0,0 +1,27 @@
+export const colors = {
+    primary: {
+        purple_100: "#36013F",
+        black_100: "#303030",
+    },
+    text: {
+        black_100: "#303030",
+        black_50: "rgba(0, 0, 0, 0.5)",
+        white_100: "#FFFFFF",
+        grey_100: "#808080",
+    },
+    accent: {
+        lightGrey_100: "#9C9C9C",
+        lightGrey_10: "rgba(156, 156, 156, 0.1)",
+        coin: "#FFC567",
+        heart: "#FF8080",
+    },
+    background: {
+        black_100: "#303030",
+        black_20: "rgba(48, 48, 48, 0.2)",
+        white_100: "#FFFFFF",
+        lightGrey_10: "rgba(156, 156, 156, 0.1)",
+        transparent: "transparent",
+        coin_10: "rgba(255, 197, 103, 0.1)",
+    },
+    error: "#F61A3D",
+};
