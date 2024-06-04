deb4a00120a74d7f2b85487a0b7ef72c4e0212bc
HGB3009 <baobaokg9@gmail.com>
2024-06-04 22:26:35 +0700

    fetching productList in Adminlistitem screen

--- CoffeeShopManagement/screens/Admin/Sales/AdminItemListScreen.js
+++ CoffeeShopManagement/screens/Admin/Sales/AdminItemListScreen.js
@@ -1,92 +1,107 @@
 import { View, Text, SafeAreaView, ScrollView, StyleSheet, FlatList } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBar from '../../../components/Client/SearchBar'
 import ItemCard from '../../../components/Admin/Card/ItemCard';
 import CategoryIcon from '../../../components/Client/Button/CategoryIcon';
+import { colors } from '../../../assets/colors/colors';
+const COFFEE_ICON = require("../../../assets/images/coffee.png");
+const JUICE_ICON = require("../../../assets/images/juice.png");
+const MILKTEA_ICON = require("../../../assets/images/milktea.png");
+const ICE_BLENDED_ICON = require("../../../assets/images/ice-blended.png");
 const PRODUCT_IMAGE_SOURCE = require("../../../assets/starbucks.jpeg");
-const GOOGLE_ICON = require("../../../assets/google.png");
-const FRUITS_ICON = require("../../../assets/fruits.png");
-const MILKTEA_ICON = require("../../../assets/milktea.png");
+import { connect } from "react-redux";
+import {
+  doc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  writeBatch,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from '../../../services/firebaseService';
+import Toast from "react-native-toast-message";
 
-const AdminItemListScreen = () => {
+
+const AdminItemListScreen = ({ userData }) => {
   const ItemCardList = [
     {
       title: "Smoothie Xoài Nhiệt Đới Granola",
       price: 65000,
       imageSource: PRODUCT_IMAGE_SOURCE,
     },
-    {
-      title: "Smoothie Phúc Bồn Tử Granola",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
-    {
-      title: "Oolong Tứ Quý Vải",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
-    {
-      title: "Oolong Tứ Quý Vải",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
-    {
-      title: "Oolong Tứ Quý Vải",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
-    {
-      title: "Oolong Tứ Quý Vải",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
-    {
-      title: "Oolong Tứ Quý Vải",
-      price: 65000,
-      imageSource: PRODUCT_IMAGE_SOURCE,
-    },
   ];
 
+  const [products, setProducts] = useState(ItemCardList);
+  useEffect(() => {
+    const loadProduct = async () => {
+      try {
+        const q = query(
+          collection(db, "products"),
+        );
+        const querySnapshot = await getDocs(q);
+        const loadedProducts = [];
+        querySnapshot.forEach((doc) => {
+          loadedProducts.push(doc.data());
+        });
+        setProducts(loadedProducts);
+      } catch (error) {
+        console.log("Error loading products:", error);
+      }
+    };
+
+    loadProduct();
+  });
+
+  const formatPrice = (price) => {
+
+    const formatter = new Intl.NumberFormat('vi-VN', {
+      style: 'currency',
+      currency: 'VND',
+    });
+    return formatter.format(price);
+  };
+
   const renderItemList = () => {
-    return ItemCardList.map((item, index) => (
+
+    return products.map((item, index) => (
       <ItemCard
         key={index}
-        title={item.title}
-        price={item.price.toLocaleString("vi-VN", {
-          style: "currency",
-          currency: "VND",
-        })}
-        imageSource={item.imageSource}
+        title={item.productName}
+        price={formatPrice(item.productPrice)}
+        imageSource={{ uri: item.productImage }}
       />
-    ));
+    )
+    );
   };
 
   const categoriesData = [
-    { type: "Category 1", iconSource: GOOGLE_ICON },
-    { type: "Category 2", iconSource: GOOGLE_ICON },
-    { type: "Category 1", iconSource: GOOGLE_ICON },
-    { type: "Category 2", iconSource: GOOGLE_ICON },
-    { type: "Category 2", iconSource: FRUITS_ICON },
-    { type: "Category 2", iconSource: FRUITS_ICON },
-    { type: "Category 2", iconSource: FRUITS_ICON },
-    { type: "Category 2", iconSource: MILKTEA_ICON },
+    { type: "Cà phê", iconSource: COFFEE_ICON },
+    { type: "Trà sữa", iconSource: MILKTEA_ICON },
+    { type: "Nước trái cây", iconSource: JUICE_ICON },
+    { type: "Đá xay", iconSource: ICE_BLENDED_ICON },
   ];
 
-  const renderCategoryItemList = ({ item }) => (
-    <CategoryIcon iconSource={item.iconSource} size={56} />
-  );
+  const renderCategoryItemList = () => {
+    return (
+      <View style={styles.categoryList}>
+        {categoriesData.map((category, index) => (
+          <CategoryIcon
+            key={index}
+            iconSource={category.iconSource}
+            size={64}
+            name={category.type}
+          />
+        ))}
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.typeRoundButtonContainer}>
-        <FlatList
-          data={categoriesData}
-          renderItem={renderCategoryItemList}
-          numColumns={4}
-          keyExtractor={(item, index) => index.toString()}
-          contentContainerStyle={styles.listContent}
-          columnWrapperStyle={styles.columnWrapper}
-        />
+      <View>
+        {renderCategoryItemList()}
       </View>
       <View style={styles.sreachBar}>
         <SearchBar />
@@ -98,7 +113,13 @@ const AdminItemListScreen = () => {
   )
 }
 
-export default AdminItemListScreen
+const mapStateToProps = (state) => {
+  return {
+    userData: state.auth.userData,
+  };
+};
+
+export default connect(mapStateToProps)(AdminItemListScreen);
 
 const styles = StyleSheet.create({
   container: {
@@ -114,10 +135,16 @@ const styles = StyleSheet.create({
     marginTop: "3%"
   },
   typeRoundButtonContainer: {
-    backgroundColor: "#ffffff",
-    borderWidth: 1,
-    borderColor: "#CCCCCC",
-    borderRadius: 20
+
+  },
+  categoryList: {
+    flexDirection: "row",
+    borderColor: colors.grey_50,
+    backgroundColor: colors.white_100,
+    justifyContent: "space-between",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: "5%"
   },
   description: {
     color: "#3A3A3A",
