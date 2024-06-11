bcefa0c84bd074d83323697da7c59f0b5beb1ee6
HGB3009 <baobaokg9@gmail.com>
2024-06-11 10:01:29 +0700

    update fetching branch List in branch management

--- CoffeeShopManagement/screens/Admin/AdminBranchManagement.js
+++ CoffeeShopManagement/screens/Admin/AdminBranchManagement.js
@@ -1,88 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, ScrollView, StyleSheet, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Branch from "../../components/Admin/Branch";
+import {
+    doc,
+    updateDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    writeBatch,
+    deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../services/firebaseService";
 
-export default function AdminBranchManagement() {
+const AdminBranchManagement = () => {
     const navigation = useNavigation();
+    const [branchList, setBranchList] = useState([]);
     const handleDetailBranch = (id) => {
         navigation.navigate("AdminBranchEditScreen", { id: id });
     };
 
-    const DATA = [
-        {
-            id: 1,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D1",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-        {
-            id: 2,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D2",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-        {
-            id: 3,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D3",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-        {
-            id: 4,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D4",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-        {
-            id: 5,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D5",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-        {
-            id: 6,
-            storeName: "Fived Coffee",
-            branchName: "HCM Đường D6",
-            distance: "3.3",
-            image: require("../../assets/images/branch1.jpg"),
-        },
-    ];
+    useEffect(() => {
+        const loadBranches = async () => {
+            try {
+                const q = query(
+                    collection(db, "branches"),
+                );
+                const querySnapshot = await getDocs(q);
+                const loadBranches = [];
+                querySnapshot.forEach((doc) => {
+                    loadBranches.push(doc.data());
+                });
+                setBranchList(loadBranches);
+            } catch (error) {
+                console.log("Error loading branches:", error);
+            }
+        };
+
+        loadBranches();
+        const unsubscribe = navigation.addListener("focus", () => {
+            loadBranches();
+        });
+
+        return unsubscribe;
+    }, [navigation]);
+
+    const renderBranchList = () => {
+        return branchList.map((item) => {
+            const address = `${item.street}, ${item.wardName}, ${item.districtName}, ${item.provinceName}`;
+            return (
+                <Branch
+                    key={item.branchId}
+                    storeName="FIVED COFFEE"
+                    branchName={item.branchName}
+                    address={address}
+                    image={{ uri: item.imageDownloadUrl }}
+                    onPress={() => handleDetailBranch(item.id)}
+                />
+            );
+        });
+    };
 
     return (
-        <View style={styles.root}>
+        <View style={styles.container}>
             <Text style={styles.header}>Danh sách chi nhánh</Text>
-            <ScrollView style={styles.scrollView}>
-                {DATA.map((item) => (
-                    <Branch
-                        key={item.id}
-                        storeName={item.storeName}
-                        branchName={item.branchName}
-                        distance={item.distance}
-                        image={item.image}
-                        onPress={() => handleDetailBranch(item.id)}
-                    />
-                ))}
+            <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+                {renderBranchList()}
             </ScrollView>
         </View>
     );
 }
 
+export default AdminBranchManagement;
+
 const styles = StyleSheet.create({
-    root: {
+    container: {
         flex: 1,
-        padding: 10,
+        paddingVertical: "3%",
+        marginHorizontal: "3%"
     },
     header: {
-        textAlign: "left",
-        fontWeight: "bold",
-        fontSize: 16,
-        marginBottom: 10,
+        color: "#3a3a3a",
+        fontSize: 18,
+        fontWeight: "600",
+        marginBottom: "3%",
     },
     scrollView: {
         flex: 1,
