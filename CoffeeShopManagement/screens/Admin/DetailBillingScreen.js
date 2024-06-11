08056903bd377a7a504a8b7036b5a94a41e912c6
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2024-06-11 23:36:50 +0700

    detail billing update

--- CoffeeShopManagement/screens/Admin/DetailBillingScreen.js
+++ CoffeeShopManagement/screens/Admin/DetailBillingScreen.js
@@ -1,217 +1,233 @@
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/Entypo'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Entypo';
+import { useNavigation } from '@react-navigation/native';
 
-export default function DetailBillingScreen() {
+export default function DetailBillingScreen({ route }) {
   const navigation = useNavigation();
-    const handleback = () => {
-        navigation.goBack();
-    }
-  const DATA = [
-    {
-      id: '12345',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-    {
-      id: '12346',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-    {
-      id: '12347',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-    {
-      id: '12348',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-    {
-      id: '12349',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-    {
-      id: '12341',
-      amount: 2,
-      name: 'Oolong Tứ Quý Kim Quất Trân Châu',
-      size: 'Vừa',
-      price: '118.000',
-    },
-  ]
+  const { orderData } = route.params;
+
+  const handleBack = () => {
+    navigation.goBack();
+  };
+
+  const totalPrice = orderData.orderTotalPrice + orderData.deliveryFee - orderData.orderTotalDiscount;
+
+  const formatCurrency = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.topApp}>
-        <TouchableOpacity
-        onPress={handleback}>
-            <Icon name='chevron-left' size={32}/>
+        <TouchableOpacity onPress={handleBack}>
+          <Icon name='chevron-left' size={32} />
         </TouchableOpacity>
-        <Text style={styles.orderText}>Đơn hàng: #####</Text>
+        <Text style={styles.orderText}>Đơn hàng: #{orderData.orderId.substring(0, 6).toUpperCase()}</Text>
       </View>
-      <ScrollView 
-      style={styles.section}
-      showsVerticalScrollIndicator={false}
-      >
-        <View style={{marginBottom: '5%'}}>
-          {DATA.map((item, index) => (
+      <ScrollView style={styles.section} showsVerticalScrollIndicator={false}>
+        <View style={styles.sectionContainer}>
+          {orderData.products.map((item, index) => (
             <View key={index} style={styles.item}>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <TouchableOpacity>
-                  <Image source={require('../../assets/system-uicons_write.png')} style={{width: 32, height: 32}}/>
-                </TouchableOpacity>
-                <View style={{justifyContent: 'space-between', marginStart: '5%'}}>
-                  <Text style={{fontSize: 12, fontWeight: '600'}}>x{item.amount} {item.name}</Text>
-                  <Text style={{color: '#808080', fontSize: 10}}>{item.size}</Text>
+              <View style={styles.itemDetails}>
+                <Image source={{ uri: item.productImage }} style={styles.productImage} />
+                <View style={styles.productInfo}>
+                  <Text style={styles.productName}>x{item.quantity} {item.productName}</Text>
+                  <Text style={styles.productSize}>{item.size}</Text>
                 </View>
               </View>
-              <Text style={{fontWeight: '600'}}>{item.price}đ</Text>
+              <Text style={styles.itemPrice}>{formatCurrency(item.totalPrice)} VND</Text>
             </View>
           ))}
         </View>
-        <View style={{marginBottom: '5%'}}>
-          <View style={styles.topInforText}>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionHeader}>
             <Image source={require('../../assets/person_icon.png')} />
-            <Text style={{marginStart: '3%', fontWeight: '600', fontSize: 16}}>Thông tin khách hàng</Text>
+            <Text style={styles.sectionText}>Thông tin khách hàng</Text>
           </View>
-          <View style={styles.inforWrapper}>
-            <Text style={{paddingBottom: '1%'}}>
-              <Text style={{fontWeight: '600'}}>Người giao: </Text>
-              <Text style={{fontWeight: '200'}}>Nguyễn Quốc Thắng</Text>
+          <View style={styles.infoWrapper}>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Người nhận: </Text>
+              <Text style={styles.contentText}>{orderData.userName}</Text>
             </Text>
-            <Text style={{paddingBottom: '1%'}}>
-              <Text style={{fontWeight: '600'}}>Số điện thoại: </Text>
-              <Text style={{fontWeight: '200'}}>0123456789</Text>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Số điện thoại: </Text>
+              <Text style={styles.contentText}>{orderData.deliveryAddress.phoneNumber}</Text>
             </Text>
-            <Text>
-              <Text style={{fontWeight: '600'}}>Địa chỉ: </Text>
-              <Text style={{fontWeight: '200'}}>Kí túc xá khu B, ...</Text>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Địa chỉ: </Text>
+              <Text style={styles.contentText}>
+                {orderData.deliveryAddress.street}, {orderData.deliveryAddress.wardName}, {orderData.deliveryAddress.districtName}, {orderData.deliveryAddress.provinceName}
+              </Text>
             </Text>
           </View>
         </View>
-        <View style={{marginBottom: '5%'}}>
-          <View style={styles.topDeliText}>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionHeader}>
             <Image source={require('../../assets/delivery_icon.png')} />
-            <Text style={{marginStart: '3%', fontWeight: '600', fontSize: 16}}>Thông tin đơn hàng</Text>
+            <Text style={styles.sectionText}>Thông tin đơn hàng</Text>
           </View>
-          <View style={styles.deliWrapper}>
-            <Text style={{paddingBottom: '1%'}}>
-              <Text style={{fontWeight: '600'}}>Người giao: </Text>
-              <Text style={{fontWeight: '200'}}>Nguyễn Quốc Thắng</Text>
+          <View style={styles.infoWrapper}>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Người giao: </Text>
+              <Text style={styles.contentText}>Nguyễn Quốc Thắng</Text>
             </Text>
-            <Text style={{paddingBottom: '1%'}}>
-              <Text style={{fontWeight: '600'}}>Số điện thoại: </Text>
-              <Text style={{fontWeight: '200'}}>0123456789</Text>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Số điện thoại: </Text>
+              <Text style={styles.contentText}>0123456789</Text>
             </Text>
-            <Text style={{paddingBottom: '1%'}}>
-              <Text style={{fontWeight: '600'}}>Mã gửi hàng: </Text>
-              <Text style={{fontWeight: '200'}}>#####</Text>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Mã gửi hàng: </Text>
+              <Text style={styles.contentText}>#####</Text>
             </Text>
-            <Text>
-              <Text style={{fontWeight: '600'}}>Mã phiếu gửi: </Text>
-              <Text style={{fontWeight: '200'}}>#####</Text>
+            <Text style={styles.infoItem}>
+              <Text style={styles.titleText}>Mã phiếu gửi: </Text>
+              <Text style={styles.contentText}>#####</Text>
             </Text>
-          </View>
-        </View>
-        <View>
-          <Text style={{fontSize: 16, fontWeight: '600'}}>BrandName Voucher</Text>
-        </View>
-        <View style={{marginBottom: '5%'}}>
-          <View style={styles.topTotalText}>
-            <Image source={require('../../assets/person_icon.png')} />
-            <Text style={{marginStart: '3%', fontWeight: '600', fontSize: 16}}>Chi tiết thanh toán</Text>
-          </View>
-          <View style={styles.totalWrapper}>
-            <View style={{paddingBottom: '1%', justifyContent: 'space-between', width: '100%', flexDirection: 'row'}}>
-              <Text style={{fontWeight: '600'}}>Tổng tiền hàng: </Text>
-              <Text style={{fontWeight: '200'}}>236.000đ</Text>
-            </View>
-            <View style={{paddingBottom: '1%', justifyContent: 'space-between', width: '100%', flexDirection: 'row'}}>
-              <Text style={{fontWeight: '600'}}>Tổng tiền phí vận chuyển: </Text>
-              <Text style={{fontWeight: '200'}}>30.000đ</Text>
-            </View>
-            <View style={{paddingBottom: '1%', justifyContent: 'space-between', width: '100%', flexDirection: 'row'}}>
-              <Text style={{fontWeight: '600'}}>Giảm giá phí vận chuyển: </Text>
-              <Text style={{fontWeight: '200'}}>-30.000đ</Text>
-            </View>
           </View>
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: '10%'}}>
-          <Text style={{fontSize: 16, fontWeight: '600'}}>Tổng tiền:</Text>
-          <Text style={{fontSize: 18, fontWeight: '600'}}>236.000đ</Text>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionHeader}>
+            <Image source={require('../../assets/delivery_icon.png')} />
+            <Text style={styles.sectionText}>BrandName Voucher</Text>
+          </View>
+          <View style={styles.sectionContainer}>
+            <View style={styles.sectionHeader}>
+              <Image source={require('../../assets/person_icon.png')} />
+              <Text style={styles.sectionText}>Chi tiết thanh toán</Text>
+            </View>
+            <View style={styles.totalWrapper}>
+              <View style={styles.totalItem}>
+                <Text style={styles.titleText}>Tổng tiền hàng: </Text>
+                <Text style={styles.contentText}>{formatCurrency(orderData.orderTotalPrice)} VND</Text>
+              </View>
+              <View style={styles.totalItem}>
+                <Text style={styles.titleText}>Tổng tiền phí vận chuyển: </Text>
+                <Text style={styles.contentText}>{formatCurrency(orderData.deliveryFee)} VND</Text>
+              </View>
+              <View style={styles.totalItem}>
+                <Text style={styles.titleText}>Giảm giá phí vận chuyển: </Text>
+                <Text style={styles.contentText}>{formatCurrency(orderData.orderTotalDiscount)} VND</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={styles.totalContainer}>
+          <Text style={styles.totalAmount}>Tổng tiền:</Text>
+          <Text style={styles.totalAmount}>{formatCurrency(totalPrice)} VND</Text>
         </View>
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-    topApp: {
-        marginTop: '10%',
-        backgroundColor: '#fff',
-        padding: '3%',
-        borderBottomWidth: 1,
-        borderColor: '#cbcbd4',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    orderText: {
-        fontSize: 16, 
-        fontWeight: '600',
-        paddingStart: '25%'
-    },
-    section: {
-        flex: 1,
-        padding: '5%',
-    },
-    item:
-    {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginBottom: '2%',
-      marginTop: '2%'
-    },
-    topInforText: {
-      flexDirection: 'row',
-      alignItems: 'center'
-    },
-    inforWrapper: {
-      backgroundColor: '#f1f7f3',
-      padding: '3%',
-      borderRadius: 10,
-    },
-    topDeliText: {
-      flexDirection: 'row',
-      alignItems: 'center'
-    },
-    deliWrapper: {
-      backgroundColor: '#f1f7f3',
-      padding: '3%',
-      borderRadius: 10,
-    },
-    topTotalText: {
-      flexDirection: 'row',
-      alignItems: 'center'
-    },
-    totalWrapper: {
-      backgroundColor: '#f1f7f3',
-      padding: '3%',
-      borderRadius: 10,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  topApp: {
+    marginTop: '10%',
+    backgroundColor: '#fff',
+    padding: '3%',
+    borderBottomWidth: 1,
+    borderColor: '#cbcbd4',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  orderText: {
+    fontSize: 18,
+    fontFamily: 'lato-bold',
+    paddingStart: '20%',
+  },
+  section: {
+    flex: 1,
+    padding: '5%',
+  },
+  sectionContainer: {
+    marginBottom: '5%',
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '2%',
+    marginTop: '2%',
+  },
+  itemDetails: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  productImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+  },
+  productInfo: {
+    justifyContent: 'space-between',
+    marginStart: '5%',
+  },
+  productName: {
+    fontSize: 16,
+    fontFamily: 'lato-bold',
+    color: '#151515',
+  },
+  productSize: {
+    color: '#808080',
+    fontSize: 16,
+    fontFamily: 'lato-bold',
+  },
+  itemPrice: {
+    fontSize: 20,
+    fontFamily: 'lato-bold',
+    color: '#151515',
+  },
+  sectionHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoWrapper: {
+    backgroundColor: '#f1f7f3',
+    padding: '3%',
+    borderRadius: 10,
+  },
+  infoItem: {
+    paddingBottom: '1%',
+  },
+  titleText: {
+    fontSize: 18,
+    fontFamily: 'lato-bold',
+    color: '#151515',
+  },
+  contentText: {
+    fontSize: 16,
+    fontFamily: 'lato-regular',
+    color: '#A3A3A3',
+  },
+  sectionText: {
+    fontSize: 18,
+    fontFamily: 'lato-bold',
+    color: '#151515',
+    marginStart: '3%',
+  },
+  totalWrapper: {
+    backgroundColor: '#f1f7f3',
+    padding: '3%',
+    borderRadius: 10,
+  },
+  totalItem: {
+    paddingBottom: '1%',
+    justifyContent: 'space-between',
+    width: '100%',
+    flexDirection: 'row',
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '10%',
+  },
+  totalAmount: {
+    fontSize: 25,
+    fontFamily: 'lato-bold',
+    color: '#151515',
+  },
+});
