90db91486782e47001dd5a282e499d309a35e60d
HGB3009 <baobaokg9@gmail.com>
2024-06-29 17:17:11 +0700

    Add Search and update Schedule Screen

--- CoffeeShopManagement/components/Admin/Modal/SelectBranchModal.js
+++ CoffeeShopManagement/components/Admin/Modal/SelectBranchModal.js
@@ -1,20 +1,43 @@
 import { View, Text, Modal, StyleSheet, TextInput, Pressable, FlatList, Image } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ModalHeader from '../../Client/Header/ModalHeader';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Branch from '../Branch';
 import BranchWithoutImageCard from '../BranchWithoutImage';
+import SearchBar from '../../Client/SearchBar';
 
 const SelectBranchModal = ({ visible, onClose, branches, setBranch }) => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [filteredBranches, setFilteredBranches] = useState(branches);
+
+  useEffect(() => {
+    handleSearch(searchKeyword);
+  }, [branches, searchKeyword]);
 
   const handleChooseBranch = (branch) => {
     setBranch(branch);
     onClose();
-  }
+  };
+
+  const handleSearch = (query) => {
+    setSearchKeyword(query);
+    if (query) {
+      const filteredList = branches.filter(branch =>
+        branch.branchName.toLowerCase().includes(query.toLowerCase()) ||
+        branch.street.toLowerCase().includes(query.toLowerCase()) ||
+        branch.wardName.toLowerCase().includes(query.toLowerCase()) ||
+        branch.districtName.toLowerCase().includes(query.toLowerCase()) ||
+        branch.provinceName.toLowerCase().includes(query.toLowerCase())
+      );
+      setFilteredBranches(filteredList);
+    } else {
+      setFilteredBranches(branches);
+    }
+  };
 
   const renderBranchList = () => {
-    return branches.map((item, index) => {
+    return filteredBranches.map((item, index) => {
       const address = `${item.street}, ${item.wardName}, ${item.districtName}, ${item.provinceName}`;
       return (
         <BranchWithoutImageCard
@@ -40,9 +63,9 @@ const SelectBranchModal = ({ visible, onClose, branches, setBranch }) => {
         <View style={styles.modalContent}>
           <ModalHeader title="Chọn chi nhánh làm việc" onClose={onClose} />
           <View style={styles.main}>
-            <View style={styles.label}>
-              <TextInput style={styles.searchText} placeholder='Tìm kiếm' />
-              <Ionicons name='search' size={20} />
+
+            <View style={styles.sreachBar}>
+              <SearchBar onChangeText={handleSearch} />
             </View>
             {renderBranchList()}
           </View>
@@ -66,6 +89,10 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     width: "90%",
     height: "70%",
+  },
+  sreachBar: {
+    marginVertical: "4%",
+    flexDirection: "row",
   },
   main: {
     height: "100%",
--- CoffeeShopManagement/screens/Admin/DetailShiftScreen.js
+++ CoffeeShopManagement/screens/Admin/DetailShiftScreen.js
@@ -5,20 +5,15 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import { useNavigation } from '@react-navigation/native';
 import StaffCard2 from '../../components/Admin/StaffCard2';
 
-export default function DetailShiftScreen({ route }) {
+const DetailShiftScreen = ({ route }) => {
 
     const staffList = route.params.selectedStaff;
     console.log(staffList);
     const navigation = useNavigation();
-    const goToEditStaff = () => {
-        navigation.navigate('EditStaff')
-    }
-    const goToAddStaff = () => {
-        navigation.navigate("AddStaff");
-    };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.topText}>Danh sách nhân viên trong ca</Text>
+            <Text style={styles.titleText}>Danh sách nhân viên trong ca</Text>
             <View style={styles.searchBox}>
                 <TextInput
                     placeholder="Tìm kiếm theo tên nhân viên, số điện thoại"
@@ -30,35 +25,21 @@ export default function DetailShiftScreen({ route }) {
                 </TouchableOpacity>
             </View>
             <ScrollView style={styles.listStaff}>
-                {staffList.map((item) => (
+                {/* {staffList.map((item) => (
                     <StaffCard2 key={item.id} item={item} />
-                ))}
+                ))} */}
             </ScrollView>
-            <TouchableOpacity
-
-                style={styles.deleteShiftButton}>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                    <Ionicons name="trash-outline" size={24} color={'#f61a3d'} />
-                    <Text
-                        style={{
-                            fontSize: 14,
-                            fontWeight: "600",
-                            marginStart: "3%",
-                            color: '#f61a3d'
-                        }}
-                    >
-                        Xoá ca làm việc
-                    </Text>
-                </View>
-            </TouchableOpacity>
         </View>
     )
 }
 
+export default DetailShiftScreen;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: '5%'
+        marginHorizontal: "3%",
+        marginVertical: "3%"
     },
     topText: {
         fontSize: 16,
@@ -89,5 +70,11 @@ const styles = StyleSheet.create({
     },
     listStaff: {
         marginBottom: '5%'
-    }
+    },
+    titleText: {
+        color: "#3a3a3a",
+        fontSize: 18,
+        fontWeight: "600",
+        fontFamily:"lato-bold"
+    },
 })
